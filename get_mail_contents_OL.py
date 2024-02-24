cf2a690364367a8533f2cfbb79390cd76c68df2d
h_takase <h.takase@portback.com>
2024-02-24 17:52:24 +0900

    02240752

--- get_mail_contents_OL.py
+++ get_mail_contents_OL.py
@@ -10,12 +10,11 @@ def main():
 
     #文字列から特定の文字列を検索し、その後の文字列を抽出する関数
 
-
     outlook = win32com.client.Dispatch("Outlook.Application")
     namespace = outlook.GetNamespace("MAPI")
     inbox = namespace.GetDefaultFolder(6)  # 受信トレイフォルダ
 
-    # "Test"フォルダを探す
+    # "FOLDER"フォルダを探す
     test_folder = None
     for folder in inbox.Folders:
         if folder.Name == FOLDER:
@@ -24,23 +23,21 @@ def main():
 
     if test_folder:
         mails = test_folder.Items
-        i = 1
         for mail in mails:
             #送信者取得
             sender = mail.SenderEmailAddress
             #特定の送信者じゃなければスルー
             if sender != SENDER_ADDR:
                 continue
-            print(f'【メール{i}】')
-            i += 1
             #件名取得
             subject = mail.Subject
             #受信時間取得
             received_time = mail.ReceivedTime.strftime("%Y-%m-%d %H:%M:%S")
             print(received_time)
-            #メール本文から複数blockを取得
+            #メール本文から複数block( 1)2)...単位)を取得
             priseString = get_prise_string.PriseString()
             blocks = priseString.get_mail_stiring_blocks(mail.Body)
+            #ブロックごとに必要なオブジェクトを取り出す
             for block in blocks:
                 object_list = priseString.get_usage_words_from_block(block)
                 if object_list:
@@ -48,11 +45,6 @@ def main():
                     opeExcel = to_excel.OpeExcel()
                     opeExcel.to_excel_from_mail(object_list)
                     del opeExcel
-            # store_info = get_usage_object(mail.Body)
-            # print(type(store_info))
-
-            # del opeExcel
-
     else:
         print("Folder 'Test' not found.")
     pass
