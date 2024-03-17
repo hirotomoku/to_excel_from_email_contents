654b07a7913e46c85214eda5f090a29e5262e111
h_takase <h.takase@portback.com>
2024-03-17 13:05:46 +0900

    環境変数の日付より受信日が新しい時のみデータ取得

--- get_mail_contents_OL.py
+++ get_mail_contents_OL.py
@@ -2,10 +2,16 @@ import re
 import win32com.client
 import to_excel
 import get_prise_string
+from datetime import datetime
+import os
+import subprocess
 
 def main():
     FOLDER = "coin"
     SENDER_ADDR = "hirotomoku@gmail.com"
+    ENV_LAST_RECEIVED_DATE = os.getenv('Last_Received_Date')
+
+    print("環境変数'A'の値:", ENV_LAST_RECEIVED_DATE)
 
 
     #文字列から特定の文字列を検索し、その後の文字列を抽出する関数
@@ -13,6 +19,10 @@ def main():
     outlook = win32com.client.Dispatch("Outlook.Application")
     namespace = outlook.GetNamespace("MAPI")
     inbox = namespace.GetDefaultFolder(6)  # 受信トレイフォルダ
+    received_time = None
+
+    #環境変数ENV_LAST_RECEIVED_DATEをdatetime型に変換
+    last_date = datetime.strptime(ENV_LAST_RECEIVED_DATE, "%Y-%m-%d %H:%M:%S")
 
     # "FOLDER"フォルダを探す
     test_folder = None
@@ -24,29 +34,29 @@ def main():
     if test_folder:
         mails = test_folder.Items
         for mail in mails:
-            #送信者取得
-            sender = mail.SenderEmailAddress
-            #特定の送信者じゃなければスルー
-            if sender != SENDER_ADDR:
-                continue
-            #件名取得
-            subject = mail.Subject
+            print("oooooooooooooooooo")
             #受信時間取得
             received_time = mail.ReceivedTime.strftime("%Y-%m-%d %H:%M:%S")
-            print(received_time)
-            #メール本文から複数block( 1)2)...単位)を取得
-            priseString = get_prise_string.PriseString()
-            blocks = priseString.get_mail_stiring_blocks(mail.Body)
-            #ブロックごとに必要なオブジェクトを取り出す
-            for block in blocks:
-                object_list = priseString.get_usage_words_from_block(block)
-                if object_list:
-                    print(f'{object_list}')
-                    opeExcel = to_excel.OpeExcel()
-                    opeExcel.to_excel_from_mail(object_list)
-                    del opeExcel
+            received_time_date = datetime.strptime(received_time, "%Y-%m-%d %H:%M:%S")
+            #受信日が環境変数の日付より古かったらスルー
+            if last_date < received_time_date:
+                print(received_time)
+                #メール本文から複数block( 1)2)...単位)を取得
+                priseString = get_prise_string.PriseString()
+                blocks = priseString.get_mail_stiring_blocks(mail.Body)
+                #ブロックごとに必要なオブジェクトを取り出す
+                for block in blocks:
+                    object_list = priseString.get_usage_words_from_block(block)
+                    if object_list:
+                        print(f'{object_list}')
+                        opeExcel = to_excel.OpeExcel()
+                        opeExcel.to_excel_from_mail(object_list)
+                        del opeExcel
     else:
         print("Folder 'Test' not found.")
+    #最新の受信日を環境変数にセット
+    command = f'setx Last_Received_Date "{received_time}"'
+    subprocess.run(command, shell=True)
     pass
 
 if __name__ == '__main__':
